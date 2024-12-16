37c6392e92ec5092368947c91d27f08bc4eabf8a
narog04 <narogsan04@gmail.com>
2024-12-16 12:14:02 +0100

    mon premier commit

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+variable = ("Hello World!")
+variable.count("i")
+len(variable)
+variable.find("!")
+variable.upper("Hello World!")     
+print("variable")
---
+++ exo1.py
@@ -0,0 +1,10 @@
+
+ch = "Véronique" 
+cr = "*"
+lc = len(ch)	
+nch = ch[0]	
+i = 1		
+while i < lc: 
+	nch = nch + cr + ch[i] 
+	i = i + 1 
+print(nch)
