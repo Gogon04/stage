9b3b0064a675f98e14826db372b78433c57962af
narog04 <narogsan04@gmail.com>
2024-12-16 15:25:25 +0100

    calculatrice

---
+++ calculatrice.py
@@ -0,0 +1,13 @@
+a = int(input("Entrer un premier nombre A: "))
+b = int(input("Entrer un second   nombre B: "))
+c = input("Entrer votre opérateur ")
+d =0
+if c == '+':
+d = a+b
+if c == '-':
+d = a-b
+if c == '*':
+d = a*b
+if c == '/':
+d = a/b
+print(d)
