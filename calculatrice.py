a68f5a653af68565eef1219dc8998a49f2b5c7bd
narog04 <narogsan04@gmail.com>
2024-12-17 11:33:23 +0100

    calculatrice améliorée

--- calculatrice.py
+++ calculatrice.py
@@ -1,12 +1,37 @@
-a = int(input("Entrer un premier nombre A: "))
-b = int(input("Entrer un second   nombre B: "))
-c = input("Entrer votre opérateur ")
+a = int(input("Entrer un premier nombre A:"))
+c = input("Entrer un premier opérateur ")
+b = int(input("Entrer un second nombre B:"))
+f = input("Entrer un second opérateur ")
+e = int(input("Entrer un troisième nombre C: "))
 d =0
+q = 0
+g = 0
+n = 0
+total = [ a,b,c,e,f ]
+chiffres = [a,b,e]
+opérateur = [c,f]
+
 if c == '+':
-    d = a + b
+    d = a + b 
 elif c == '-':
     d = a - b
 elif c == '*':
-    d = a * b
+    d = a * b 
 elif c == '/':
-print(d)
+    d = a / b
+if f == '+' :
+    n = d + e
+elif f == '-':
+    n = d - e
+elif f == '/':
+    n = d / e
+elif f == '*':
+    n = d*e
+print(n)   
+
+
+
+for p in chiffres :
+    q+=1
+for z in opérateur :
+    g+=1
--- essai.py
+++ essai.py
@@ -1,12 +1,7 @@
-# Demande à l'utilisateur d'entrer deux nombres et un opérateur
 a = int(input("Entrer un premier nombre A: "))
 b = int(input("Entrer un second nombre B: "))
-c = input("Entrer votre opérateur (+, -, *, /): ")
-
-# Initialisation de la variable d
+c = input("Entrer votre opérateur:") 
 d = 0
-
-# Vérification de l'opérateur et calcul
 if c == '+':
     d = a + b
 elif c == '-':
@@ -14,15 +9,5 @@ elif c == '-':
 elif c == '*':
     d = a * b
 elif c == '/':
-    if b != 0:  # Vérification pour éviter la division par zéro
-        d = a / b
-    else:
-        print("Erreur: Division par zéro.")
-        d = None
-else:
-    print("Erreur: Opérateur non valide.")
-    d = None
-
-# Affichage du résultat
-if d is not None:
-    print("Le résultat est:", d)
+    d= a/b
+print(d)    
