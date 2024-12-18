190eea732209822d4f25cd329ce9049c1cbf10ff
narog04 <narogsan04@gmail.com>
2024-12-18 09:58:02 +0100

    rétrécis

--- exo.py
+++ exo.py
@@ -1,11 +1,10 @@
 a=(input("Choisissez un mot:"))
 t=0
-l=0
-c=a
-t= t+1
+t=+1
 if "e" in a:
   print("Le mot contient la lettre e")
 if "e" not in a:
   print("Le mot choisis ne contient pas lettre e")
   if "e" in a:
-    print(len(t))
+     for z in t :
+       print(len (t))
